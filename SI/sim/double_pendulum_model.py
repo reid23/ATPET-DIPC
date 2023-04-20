c35f6e18fb5e2f8774b27c25800a556c5e75cb23
ReidDye <40401995+reid23@users.noreply.github.com>
2023-04-19 20:45:40 -0500

    sp.solve is soooooo slow

--- SI/sim/double_pendulum_model.py
+++ SI/sim/double_pendulum_model.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import control as ct
 import matplotlib.animation as animation
-from numba import njit
+# from numba import njit
 #             ----> x
 # ___________III______
 #             \   |
@@ -35,53 +35,10 @@ from numba import njit
 
 # func = sp.lambdify([y, F, lb, lc, lcom, c, g, ma, mb, mc, IBzz, ICzz], ydot)
 #%%
-@njit
+# @njit
 def eval_fourier(t, weights):
     return np.array([1]+[np.cos(n*t) for n in range(1, 50)]+[np.sin(n*t) for n in range(1, 50)])@weights
 class dipc_model:
-    fourier_weights = np.array([-1.01059992, -5.15912399,  0.62330237,  3.05759723, -1.12530636,
-        2.70284226, -4.50405468,  0.28368342,  3.24455156, -5.35317389,
-        3.10959512, -5.81990927,  1.45885037,  5.02717026,  0.97317233,
-       -5.30639522, -5.30537116, -5.74147928, -5.90698523,  4.36302429,
-       -1.87339229,  3.72996092,  5.44033578, -4.19383407,  3.49324388,
-       -3.74749909, -3.10188143, -1.38929699,  5.45965118, -1.14199923,
-       -1.87365414,  2.05730946, -0.74948988,  2.88238477,  5.3534978 ,
-       -0.59731288, -2.68040456,  2.17956034,  5.77212432,  3.89544017,
-       -1.44203953,  0.19349814,  0.0610279 , -3.5676886 ,  3.82201409,
-        2.77510269, -2.73921327, -2.8959084 , -4.42520525,  3.39951133,
-        0.20293385,  4.02532746,  5.37061355,  4.05480086, -1.28061653,
-       -2.10952495, -5.78233763, -2.77562075,  4.59822595,  2.28902516,
-        2.91955732,  1.15328398,  2.89499228,  0.90932704, -3.17843466,
-       -0.17923359, -1.7568002 ,  5.8974776 ,  5.54273894,  1.2979972 ,
-       -1.32922312,  4.24627956, -2.28375478, -0.47280421,  3.07124023,
-       -2.24965711,  5.40198739,  1.11280345, -0.98245419,  5.93988155,
-        1.76381701, -1.80283728,  5.52826215, -2.60865246,  4.02604869,
-        0.87696243,  3.0560031 ,  5.042354  , -5.79265693, -0.13775956,
-       -3.17305324, -0.35643387,  0.94615916, -4.0704026 , -5.48553411,
-        2.27536486, -0.32290616,  5.9038042 ,  5.16601646])
-    ff_path = np.array([ 9.84133178e-03, -1.37981774e-04, -3.41530340e-03, -2.51843290e-08,
-        1.17409120e-08,  7.69891949e-04, -3.97038688e-03, -3.34800518e-08,
-       -3.25464305e-08,  9.53878506e-13, -2.93953002e-14,  7.38233369e-13,
-        2.73852493e-15, -3.29089743e-13, -6.18642696e-14, -7.22536599e-14,
-       -8.49240943e-14,  1.27940816e-13, -7.24897440e-13,  2.58591411e-13,
-       -4.53016736e-13, -2.04762762e-13, -1.23002510e-13, -1.05671607e-13,
-        5.53440758e-03, -3.86502371e-13,  4.11185959e-10, -4.26621904e-13,
-        6.10836677e-13, -4.72457331e-14, -5.48102077e-13, -1.66754174e-13,
-        4.72834320e-17, -5.72465668e-13,  6.45213359e-13, -7.81809334e-13,
-        4.90981537e-13,  5.68689720e-13, -6.13978869e-13,  1.88572428e-13,
-        0.00000000e+00,  0.00000000e+00,  4.00537664e-13,  2.34027836e-13,
-       -2.87178212e-13,  1.69502661e-13,  2.78644422e-13,  1.22674576e-13,
-        1.62503241e-13,  0.00000000e+00,  3.29257519e-13, -1.39694279e-13,
-        2.85864398e-13,  3.27273753e-15,  4.93918434e-06,  2.92334947e-13,
-       -1.83803349e-13, -5.42774322e-14,  2.17378534e-14,  3.34277020e-04,
-       -6.42325638e-14,  4.32368726e-14, -8.84487515e-13,  1.16364233e-13,
-       -1.73115321e-13, -6.60862984e-13, -2.37121220e-04,  0.00000000e+00,
-       -7.37475162e-13, -9.25642545e-14, -3.57962026e-13,  1.14446795e-04,
-       -2.21508689e-13, -1.33152916e-04, -8.12170564e-13,  7.95597127e-14,
-       -4.65881486e-13, -1.71569117e-13, -4.21710013e-15, -2.67467577e-13,
-       -1.53194506e-13, -3.43884938e-13,  3.40466933e-14,  6.73865169e-15,
-       -1.13626826e-13, -2.46224758e-05,  0.00000000e+00, -1.60802578e-08,
-       -1.27854804e-14, -3.14870994e-15,  1.21924364e-06])
        #                                                            0.084            0.0007621
     def __init__(self, constants=[0.3, 0.2,   0.5,   1, 9.81, 0.25, 0.125, 0.125, 0.00065, 0.00065]):
         self.K = {}
@@ -125,7 +82,7 @@ class dipc_model:
 
         self.F = dynamicsymbols('F')
         self.cart.apply_force(self.F*self.cart.x) # motor
-        self.cart.apply_force(-self.c*self.y[3]*self.cart.x, reaction_body=self.track) # friction
+        # self.cart.apply_force(-self.c*self.y[3]*self.cart.x, reaction_body=self.track) # friction
 
         # gravity
         self.cart.apply_force(-self.track.y*self.cart.mass*self.g)
@@ -136,6 +93,10 @@ class dipc_model:
         self.method = JointsMethod(self.track, self.slider, self.rev1, self.rev2)
         self.method.form_eoms()
         self.ydot = self.method.rhs()
+
+        a = sp.Symbol('a')
+        print('ydot[3]:')
+        print(sp.solve(sp.Eq(self.ydot[3], a), self.F), open("sp_soln.txt", "w"))
     def linearize(self, op_point=[0,sp.pi,0,0,0,0]):
         op_point=dict(zip(
             self.y+[self.F,
@@ -272,10 +233,19 @@ if __name__ == '__main__':
     Q = np.diag([100, 10, 5, 1, 100, 50])
     R = np.diag([10])
     model = dipc_model().linearize().lambdify()
+    exit()
     # .construct_PP(eigs).construct_LQR(Q, R).integrate_with_scipy(y_0 = [0,0,0,0,0,0], targets = np.array([[  0, np.pi, 0, 0, 0, 0]]), target_timestamps = [0, 1.5], controller = 'FFBF')
     # model.print_eoms()
+    print("ydot:")
+    print(model.ydot)
+    print("f:")
+    a = sp.Symbol('a')
+    print(sp.solve(sp.Eq(model.ydot[3], a), model.F, simplify=False))
+    # print(sp.simplify(sp.trigsimp(sp.expand(sp.solve(model.ydot[3], model.F)))))
+
+    # print(sp.simplify(sp.trigsimp(sp.expand(model.ydot))))
     normal = model.ydot.subs(zip([model.lb, model.lc, model.lcom, model.c, model.g, model.ma, model.mb, model.mc, model.IBzz, model.ICzz], model.constants))
-    print(normal)
+    # print(normal)
     # print(systems.dsolve_system([sp.Eq(model.y[i].diff(model.t), normal[i]) for i in range(6)], 
     # ics=dict(zip([i.subs(model.t, 0) for i in model.y]+[i.subs(model.t, 1.5) for i in model.y],[0, 0, 0, 0, 0, 0]+[0,sp.pi,0,0,0,0]))))
     print(systems.dsolve_system([sp.Eq(model.y[i].diff(model.t), normal[i]) for i in range(6)]))
