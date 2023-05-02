bcc59e3550abdf03e6bc15a6114ba62868f3b2c2
ReidDye <40401995+reid23@users.noreply.github.com>
2023-05-01 22:59:54 -0700

    finished making nice graphs

--- nice_graphs.py
+++ nice_graphs.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # mpc
-with open('final_data_for_plotting_mpc.txt') as f:
-    data = np.array(eval(f.read()))[5000:]
-t = np.cumsum(data[:, 0]+0.00000035)
-x, th, dx, dth, u = data[:, (1,2,4,5,7)].T
+# with open('final_data_for_plotting_mpc.txt') as f:
+#     data = np.array(eval(f.read()))[5000:]
+# t = np.cumsum(data[:, 0]+0.00000035)
+# x, th, dx, dth, u = data[:, (1,2,4,5,7)].T
 
 # LQR
-# with open('LQR_demo_data_down.txt') as f:
-#     data = np.array(eval(f.read()))[1800:]
-# t = data[:, 0]/1_000_000_000
-# x, th, dx, dth, u = data[:, (2,3,5,6,7)].T
+with open('LQR_demo_data_up.txt') as f:
+    data = np.array(eval(f.read()))[1088:2120]#[1800:]
+t = data[:, 0]/1_000_000_000
+x, th, dx, dth, u = data[:, (2,3,5,6,7)].T
 
 fig, ax = plt.subplots(3, 1, sharex=True)
 
@@ -20,9 +20,9 @@ ax[0].plot(t, x, label='$x$ (m)')
 ax[0].plot(t, dx, label='$\dot x$ (m/s)')
 
 ax[1].set_title('Pendulum Angle and Angular Velocity')
-ax[1].plot(t, (th)%(2*np.pi), label='$\\theta$ (rad) (mod 2pi)')
+ax[1].plot(t, (th)%(2*np.pi), label='$\\theta$ (rad)')
 # ax[1].plot(t, th, label='$\\theta$ (rad)')
-ax[1].plot(t, dth/5, label='$\dot \\theta$ (x5 rad/s)')
+ax[1].plot(t, dth, label='$\dot \\theta$ (rad/s)')
 
 ax[2].set_title('Control Input (Cart Acceleration)')
 ax[2].plot(t, u, label='$u$ (m/s^2)')
@@ -31,7 +31,7 @@ ax[0].legend()
 ax[1].legend()
 ax[2].legend()
 
-fig.suptitle('Pendulum State over Time: MPC Swing-Up')
+fig.suptitle('Pendulum State over Time: LQR Up')
 
 # plt.legend()
 plt.xlabel('Time (s)')
